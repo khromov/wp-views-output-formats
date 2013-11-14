f7a3e0433ed4d900820fdd6df22bfb8547302b8e
khromov <stanislav.khromov@gmail.com>
2013-11-14 23:53:43 +0100

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== Views Output Formats ===
+Contributors: khromov
+Tags: views, wp_query, query, xml, json
+Requires at least: 3.6
+Tested up to: 3.7
+Stable tag: 1.0
+License: GPL2
+
+Export your WordPress data in XML and JSON formats easily!
+
+== Description ==
+This plugin provides JSON and XML output formats for Toolset Views.
+
+The plugin will also fetch all available custom fields for each post, making it truly simple to integrate WordPress
+with other systems that speak JSON and XML.
+
+**Usage**
+
+*Basic usage*
+
+Create your View, go to Settings -> Views Output Formats and pick your format to get a link!
+
+== Requirements ==
+* PHP 5.3 or higher
+
+== Translations ==
+* None
+
+== Installation ==
+1. Upload the `views-output-formats` folder to `/wp-content/plugins/`
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Create your View using Toolset Views, go to Settings -> Views Output Formats and pick your format to get a link!
+
+== Frequently Asked Questions ==
+
+= What are the prerequisites for using this plugin? =
+
+You need to have the Toolset Views plugin installed.
+
+More information can be found here:
+
+http://wp-types.com/home/views-create-elegant-displays-for-your-content/
+
+== Screenshots ==
+
+1. Administration menu
+2. Exported XML example
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+= 1.0 =
+Initial release
+
+== Upcoming features ==
+
+* Support for getting post taxonomies
+* Support for user export (New feature in Views 1.4)
+
---
+++ wp-views-output-formats.php
@@ -0,0 +1,264 @@
+<?php
+/*
+Plugin Name: Views Output Formats
+Plugin URI: http://wordpress.org/extend/plugins/views-output-formats
+Description: Provides JSON and XML output formats for Toolset Views
+Version: 1.0
+Author: khromov
+Author URI: http://khromov.wordpress.com
+License: GPL2
+*/
+
+class Views_Output_Formats
+{
+	const text_domain = "vof";
+
+	function __construct()
+	{
+		/**
+		 * Translation support
+		 **/
+		load_plugin_textdomain(self::text_domain, false, basename( dirname( __FILE__ ) ) . '/languages' );
+
+		/**
+		 * Register hooks, languages etc
+		 **/
+		register_activation_hook(__FILE__ , array( 'Views_Output_Formats', 'activate' ));
+		register_deactivation_hook(__FILE__ , array( 'Views_Output_Formats', 'deactivate' ));
+
+		/**
+		 * Register menu item
+		 */
+		add_action('admin_menu', array( &$this, 'register_admin_menus'));
+
+		/**
+		 * Add URL endpoints for accessing different formats
+		 */
+		add_filter('query_vars', array(&$this, 'register_vars'));
+		add_action('wp', array(&$this, 'register_endpoints')); //pre_get_posts is faster, but we can't use some functions there...
+	}
+
+	function register_vars($vars)
+	{
+		$vars[] = 'vof_id';
+		$vars[] = 'vof_format';
+		$vars[] = 'vof_token';
+		return $vars;
+	}
+
+	function register_endpoints($query)
+	{
+		//Sanity checks
+		if(	get_query_var('vof_id') !== '' 		&&
+			get_query_var('vof_token') !== '' 	&&
+			get_option('vof_token') !== false 	&&
+			get_option('vof_token') === get_query_var('vof_token') &&
+			get_post_type((int)get_query_var('vof_id')) === 'view' &&
+			function_exists('wpv_filter_get_posts')
+		)
+		{
+			//Pick format
+			$format = (get_query_var('vof_format') === 'xml') ? 'xml' : 'json';
+
+			//Query results
+			$query_result = wpv_filter_get_posts((int)get_query_var('vof_id'));
+
+			//Add in custom fields
+			$posts_fixed['post'] = $this->views_output_merge_custom_fields($query_result->posts);
+
+			//TODO: Add in taxonomies
+			//$posts_fixed['post'] = views_output_merge_taxonomies($query_result->posts);
+
+			//Finalize array
+			$posts_finished = array('posts' => $posts_fixed);
+
+			if($format === 'json')
+			{
+				//Porque you no work?
+				//status_header('Content-type: application/json');
+
+				header('Content-type: application/json');
+				echo json_encode($posts_finished);
+			}
+			else
+			{
+				//status_header('Content-Type:text/xml');
+				header('Content-Type: text/xml');
+
+				//Do some additional transformation to get the output format we want
+				$posts_tmp = array();
+				$i = 0;
+				foreach($posts_finished as $post)
+				{
+					foreach($post['post'] as $post_inner)
+					{
+						$posts_tmp['posts'][$i] = $post_inner;
+						$i++;
+					}
+				}
+
+				echo Views_Output_Formats_XMLSerializer::generateValidXmlFromArray($posts_tmp, '', 'post');
+			}
+
+			die(); //Early return
+		}
+	}
+
+	function register_admin_menus()
+	{
+		add_submenu_page( 'options-general.php', __( "Views Output Formats", self::text_domain), __( "Views Output Formats", self::text_domain ), 'manage_options', 'vof', array( &$this, 'admin_main' ));
+	}
+
+	function admin_main()
+	{
+		$views = $this->get_all_views();
+		$views_html_output = '<table>';
+		/* WP_Query $views */
+		foreach($views->get_posts() as $view)
+		{
+			/* @var WP_Post $view */
+			$views_html_output .= '
+									<tr>
+										<td>
+											'. $view->post_title .'
+										</td>
+										<td>
+											<a href="'. get_bloginfo('url') .'/?vof_id='.$view->ID.'&vof_format=xml&vof_token='. get_option('vof_token') .'">XML</a>
+											|
+											<a href="'. get_bloginfo('url') .'/?vof_id='.$view->ID.'&vof_format=json&vof_token='. get_option('vof_token') .'">JSON</a>
+										</td>
+									</tr>
+									';
+		}
+
+		$views_html_output .= '</table>';
+
+		ob_start();
+		?>
+		<div class="wrap">
+			<?php screen_icon(); ?>
+			<h2>
+				<?php _e( 'Views Output Formats', 'dfwmdt' ); ?>
+			</h2>
+
+			<div class="info" style="padding-top: 10px;">
+				<?php _e( 'From this screen you can get the URL links for all your Views in XML or JSON formats.', self::text_domain ); ?>
+				<p>
+					<?php _e('Your secret', self::text_domain); ?> <abbr title="<?php _e('The token can be reset by deactivating and reactivating the plugin.', self::text_domain); ?>"><?php _e('API token',self::text_domain); ?> </abbr> <?php _e('is:', self::text_domain); ?> <strong><?php echo get_option('vof_token'); ?></strong> <br/>
+				</p>
+				<p>
+					<?php echo $views_html_output; ?>
+				</p>
+			</div>
+		</div>
+		<?php
+		echo ob_get_clean();
+	}
+
+	/**
+	 * Returns a list of all views
+	 */
+	function get_all_views()
+	{
+		$args = array('post_type' => 'view', 'order' => 'ASC', 'posts_per_page' => -1, 'post_status' => 'publish');
+		return new WP_Query($args);
+	}
+
+	/** Adds custom fields to the post and returns it */
+	function views_output_merge_custom_fields($posts)
+	{
+		$posts = array_map(array(&$this, 'views_add_custom_fields_for_post_map'), $posts);
+		return $posts;
+	}
+
+	function views_add_custom_fields_for_post_map($post, $exclude_hidden_fields = 1)
+	{
+		//Aggregate custom fields for the post
+		$custom_fields = get_post_custom($post->ID);
+
+		$post->custom_fields = array();
+
+		//Main Custom Field loop
+		foreach($custom_fields as $key => $value)
+		{
+			if(sizeof($custom_fields[$key]) === 1)
+				$post->custom_fields[$key] = $value[0];
+			else
+			{
+				for($i = 0; $i < sizeof($value); $i++)
+					$post->custom_fields[$key]['value_'.($i+1)] = $value[$i];
+			}
+		}
+		return $post;
+	}
+
+	/**
+	 * Plugin activation function
+	 **/
+	static function activate()
+	{
+		//Create a random API token
+		$token = md5(wp_generate_password());
+		add_option('vof_token', $token);
+	}
+
+	/**
+	 * Plugin deactivation function
+	 */
+	static function deactivate()
+	{
+		//If we re-initialize the plugin, the API key will be changed
+		delete_option('vof_token');
+	}
+}
+
+class Views_Output_Formats_XMLSerializer
+{
+	//functions adopted from http://www.sean-barton.co.uk/2009/03/turning-an-array-or-object-into-xml-using-php/
+	public static function generateValidXmlFromObj(stdClass $obj, $node_block='nodes', $node_name='node')
+	{
+		return self::generateValidXmlFromArray(get_object_vars($obj), $node_block, $node_name);
+	}
+
+	public static function generateValidXmlFromArray($array, $node_block='nodes', $node_name='node')
+	{
+		$xml = '<?xml version="1.0" encoding="UTF-8" ?>';
+
+		//$xml .= '<' . $node_block . '>';
+		$xml .= self::generateXmlFromArray($array, $node_name);
+		//$xml .= '</' . $node_block . '>';
+
+		return $xml;
+	}
+
+	private static function generateXmlFromArray($array, $node_name)
+	{
+		$xml = '';
+
+		if (is_array($array) || is_object($array))
+		{
+			foreach ($array as $key => $value)
+			{
+				if (is_numeric($key))
+				{
+					$key = $node_name;
+				}
+
+				//Special case for outer wrapper
+				//if($node_name === false)
+				//	$xml .= self::generateXmlFromArray($value, $node_name);
+				//else
+				$xml .= '<' . $key . '>' . self::generateXmlFromArray($value, $node_name) . '</' . $key . '>';
+			}
+		}
+		else
+		{
+			$xml = htmlspecialchars($array, ENT_QUOTES);
+		}
+
+		return $xml;
+	}
+}
+
+//Init plugin
+$vof = new Views_Output_Formats();
